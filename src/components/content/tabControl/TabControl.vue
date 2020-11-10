2e20e2b70f6a70b1d44666e8d311025a041497d9
Garfield_zhang <929780650@qq.com>
2020-11-10 15:08:39 +0800

    tabControl done

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="tabcontrol">
+    <div class="tab-item" v-for="(item, index) in titles" v-bind:key="index" @click="changeTab(index)">
+      <span :class="{active: index===currentIndex}">{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabControl',
+  props:{
+    titles:{
+      type: Array,
+      default(){
+        return [];
+      }
+    }
+  },
+
+  data() {
+    return {
+      currentIndex: 0
+    }
+  },
+
+
+  methods: {
+    changeTab(e){
+    //  console.log(e);
+      this.currentIndex = e;
+    }
+  },
+}
+</script>
+
+<style>
+
+.tabcontrol{
+  display: flex;
+  width: 100%;
+  height: 50px;
+  justify-content: center;
+}
+
+.tab-item{
+  flex: 1;
+  text-align: center;
+  font-size: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.tab-item span{
+  width: 80px;
+  padding-bottom: 10px;
+
+}
+
+.active{
+  border-bottom: 5px solid #1A7BBB;
+}
+
+
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,50 +1,103 @@
 <template>
-  <div id='home'>
-    <navi-bar class='home-navi'><div slot='mid'>购物街</div></navi-bar>
+  <div id="home">
+    <navi-bar class="home-navi"><div slot="mid">购物街</div></navi-bar>
     <home-swiper :banners="banners"></home-swiper>
     <recommends :recommends="recommends"></recommends>
+    <tab-control :titles="titles"></tab-control>
+    <ul>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+      <li>test</li>
+    </ul>
   </div>
 </template>
 
 <script>
 import NaviBar from "components/common/navibar/NaviBar";
-import HomeSwiper from "./ChildComp/HomeSwiper"
-import Recommends from "./ChildComp/Recommends"
+import HomeSwiper from "./ChildComp/HomeSwiper";
+import Recommends from "./ChildComp/Recommends";
+import TabControl from "components/content/tabControl/TabControl"
 
-import {getHomeMultidata} from "network/home.js"
+import { getHomeMultidata } from "network/home.js";
 
 export default {
   name: "Home",
 
-
   components: {
     NaviBar,
     HomeSwiper,
-    Recommends
+    Recommends,
+    TabControl
   },
 
-
-  data(){
+  data() {
     return {
       banners: [],
-      recommends: []
-    }
+      recommends: [],
+      titles: ["精选","新款","流行"]
+    };
   },
 
-
-  created(){
+  created() {
     let that = this;
-    getHomeMultidata().then( res => {
+    getHomeMultidata().then((res) => {
       that.banners = res.data.banner.list;
-      that.recommends = res.data.recommend.list
-    })
-  }
+      that.recommends = res.data.recommend.list;
+    });
+  },
 };
 </script>
 
 <style>
-.home-navi{
-  background-color: rgb(18,150,219);
+
+#home{
+  padding-top: 44px;
+}
+
+.home-navi {
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  background-color: rgb(18, 150, 219);
   color: #ffffff;
+  z-index: 9;
 }
 </style>
