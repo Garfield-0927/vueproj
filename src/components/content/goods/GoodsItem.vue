0dff35ad18339d0689d65eb92f3d79a5f20d4943
Garfield_zhang <929780650@qq.com>
2020-11-16 23:01:30 +0800

    css adjust

--- src/components/content/goods/GoodsItem.vue
+++ src/components/content/goods/GoodsItem.vue
@@ -3,8 +3,10 @@
     <img class="itemImage" :src="goodsItem.show.img" alt="" />
     <div>
       <p class="title">{{ goodsItem.title }}</p>
-      <span class="price">{{ goodsItem.price }}</span>
-      <span class="favorites">{{ goodsItem.cfav }}</span>
+      <div class="priceAndfav">
+        <span class="price">$ {{ goodsItem.price }}</span>
+        <span class="favorites">{{ goodsItem.cfav }}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -31,6 +33,7 @@ export default {
 .itemwrapper {
   width: 50%;
   display: inline-block;
+  margin-top: 10px;
 }
 
 .itemImage {
@@ -41,5 +44,21 @@ export default {
   text-overflow: ellipsis;
   overflow: hidden;
   white-space: nowrap;
+}
+
+.priceAndfav{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.price{
+  text-align: center;
+  flex: 1;
+}
+
+.favorites{
+  text-align: center;
+  flex: 1;
 }
 </style>
