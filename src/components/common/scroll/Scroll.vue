5afc6137ed1fa39d5a3787aab2073b1692ac8ad2
Garfield_zhang <929780650@qq.com>
2020-11-21 19:46:59 +0800

    bscroll

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,30 @@
+<template>
+  <div ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from "better-scroll";
+
+export default {
+  name: "Scroll",
+  data() {
+    return {
+      scroll: null,
+    };
+  },
+  mounted() {
+    this.scroll = new BScroll(this.$refs.wrapper, {
+      click: true
+    });
+
+
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,52 +1,16 @@
 <template>
   <div id="home">
     <navi-bar class="home-navi"><div slot="mid">购物街</div></navi-bar>
-    <home-swiper :banners="banners"></home-swiper>
-    <recommends :recommends="recommends"></recommends>
-    <tab-control class="tab-control" :titles="titles" @tabClick='tabclick'></tab-control>
-    <goods :goods="goods[currentType].list"></goods>
-    <ul>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-      <li>test</li>
-    </ul>
+    <scroll class="wrapper">
+      <home-swiper :banners="banners"></home-swiper>
+      <recommends :recommends="recommends"></recommends>
+      <tab-control
+        class="tab-control"
+        :titles="titles"
+        @tabClick="tabclick"
+      ></tab-control>
+      <goods :goods="goods[currentType].list"></goods>
+    </scroll>
   </div>
 </template>
 
@@ -55,7 +19,8 @@ import NaviBar from "components/common/navibar/NaviBar";
 import HomeSwiper from "./ChildComp/HomeSwiper";
 import Recommends from "./ChildComp/Recommends";
 import TabControl from "components/content/tabControl/TabControl";
-import Goods from "components/content/goods/Goods"
+import Goods from "components/content/goods/Goods";
+import Scroll from "components/common/scroll/Scroll";
 
 import { getHomeMultidata, getHomeGoods } from "network/home.js";
 
@@ -68,6 +33,7 @@ export default {
     Recommends,
     TabControl,
     Goods,
+    Scroll,
   },
 
   data() {
@@ -80,7 +46,7 @@ export default {
         new: { page: 0, list: [] },
         sell: { page: 0, list: [] },
       },
-      currentType: 'sell',
+      currentType: "sell",
     };
   },
 
@@ -94,29 +60,25 @@ export default {
     this.getHomeGoods("new");
   },
 
+  mounted() {},
 
   methods: {
-    tabclick(index){
+    tabclick(index) {
       // console.log(index);
-      switch (index){
+      switch (index) {
         case 0:
-          this.currentType = 'sell';
+          this.currentType = "sell";
           break;
         case 1:
-          this.currentType = 'new';
+          this.currentType = "new";
           break;
         case 2:
-          this.currentType = 'pop';
+          this.currentType = "pop";
           break;
         default:
           break;
       }
     },
-
-
-
-
-
 
     getHomeMultidata() {
       getHomeMultidata().then((res) => {
@@ -127,27 +89,25 @@ export default {
 
     getHomeGoods(type) {
       let page = this.goods[type].page + 1;
-      getHomeGoods(type, page).then( res => {
-        // console.log(res);
-        this.goods[type].list.push(...res.data.list);
-        this.goods[type].page+=1;
-      }, err => {
-        console.log(err);
-      })
+      getHomeGoods(type, page).then(
+        (res) => {
+          // console.log(res);
+          this.goods[type].list.push(...res.data.list);
+          this.goods[type].page += 1;
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
     },
-
-
-
   },
-
-
-
 };
 </script>
 
-<style>
+<style scoped>
 #home {
   padding-top: 44px;
+  height: 100vh;
 }
 
 .home-navi {
@@ -163,5 +123,12 @@ export default {
 .tab-control {
   position: sticky;
   top: 44px;
+}
+
+.wrapper {
+  height: calc(100% - 93px);
+  overflow: hidden;
+  position: fixed;
+  width: 100%;
 }
 </style>
