0598dbbed79407c3c3e583803585987ecc9cfaa0
Garfield_zhang <929780650@qq.com>
2020-12-04 16:55:58 +0800

    get swiperImg of detail page

---
+++ src/views/detail/ChildComp/DetailSwiper.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+  <swiper>
+    <swiper-item v-for="(item, index) in swiperImg" :key="index">
+      <img :src="item" alt="">
+    </swiper-item>
+  </swiper>
+  </div>
+</template>
+
+<script>
+import Swiper from "@/components/common/swiper/Swiper";
+import SwiperItem from "@/components/common/swiper/SwiperItem";
+
+export default {
+  name: "DetailSwiper",
+  components: {
+    SwiperItem,
+    Swiper
+  },
+  props: {
+    swiperImg:{
+      type: Array,
+      default(){
+        return [];
+      }
+    }
+  },
+
+  mounted() {
+    setTimeout(()=>{
+      console.log(this.swiperImg)
+
+    }, 500)
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,29 +1,45 @@
 <template>
   <div>
     <detail-navi-bar></detail-navi-bar>
+    <detail-swiper :swiper-img="swiperImg"></detail-swiper>
   </div>
 
 </template>
 
 <script>
 import DetailNaviBar from "@/views/detail/ChildComp/DetailNaviBar";
-
+import {getDetail} from "network/detail.js"
+import DetailSwiper from "@/views/detail/ChildComp/DetailSwiper";
   export default {
     name: "Detail",
     components:{
-      DetailNaviBar
+      DetailNaviBar,
+      DetailSwiper,
+
     },
     data(){
       return{
-        iid: null
+        iid: null,
+        swiperImg: null,
+
       }
     },
 
     created() {
       this.iid = this.$route.params.iid;
+
+      // get detail of goods
+      getDetail(this.iid).then(res =>{
+        console.log(res);
+        this.swiperImg = res.result.itemInfo.topImages;
+        console.log(this.swiperImg)
+      })
+
+
     },
 
     deactivated() {
+      // 离开时销毁组件 否则会keep-alive
       this.$destroy('Detail');
     },
 
